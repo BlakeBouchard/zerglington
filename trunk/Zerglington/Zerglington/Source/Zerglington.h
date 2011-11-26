f0c2d9c8d4c46e6963672db76766cc2204bb4b70
thatsblake@gmail.com <thatsblake@gmail.com@e11327cd-bf1a-9caf-8538-91d26e9a85f1>
2011-11-26 19:33:25 +0000

    - Moved BWTA code to Scouter from Zerglington

--- trunk/Zerglington/Zerglington/Source/Zerglington.h
+++ trunk/Zerglington/Zerglington/Source/Zerglington.h
@@ -1,19 +1,12 @@
 #pragma once
 #include <BWAPI.h>
+#include "Scouter.h"
 #include "Worker.h"
 
-//#include <BWTA.h>
-#include <windows.h>
 #include <queue>
 #include <map>
 
 enum morph{DRONE, POOL};
-
-//extern bool analyzed;
-//extern bool analysis_just_finished;
-//extern BWTA::Region* home;
-//extern BWTA::Region* enemy_base;
-//DWORD WINAPI AnalyzeThread();
 
 class Zerglington : public BWAPI::AIModule
 {
@@ -37,7 +30,6 @@ public:
 	void drawStats(); //not part of BWAPI::AIModule
 	void drawBullets();
 	void drawVisibilityData();
-	//void drawTerrainData();
 	void showPlayers();
 	void showForces();
 	bool show_bullets;
