354b2d9c7e6a4db8c4cd6be3ae7c9af2adc48a02
thatsblake@gmail.com <thatsblake@gmail.com@e11327cd-bf1a-9caf-8538-91d26e9a85f1>
2011-11-27 21:03:39 +0000

    - Rudimentary attack code implemented

--- Zerglington/Zerglington/Source/Striker.cpp
+++ Zerglington/Zerglington/Source/Striker.cpp
@@ -1,11 +1,51 @@
 #include "Striker.h"
 
+using namespace BWAPI;
+using namespace std;
 
 Striker::Striker(void)
 {
+	initialized = false;
 }
-
 
 Striker::~Striker(void)
 {
+}
+
+void Striker::initialize(TilePosition base)
+{
+	addAllZerglings();
+	setEnemyBase(Position(base));
+	initialized = true;
+}
+
+void Striker::addZergling(Unit* unit)
+{
+	strikers.push_back(unit);
+}
+
+void Striker::addAllZerglings(void)
+{
+	set<Unit*> allUnits = Broodwar->getAllUnits();
+	for (set<Unit*>::iterator i = allUnits.begin(); i != allUnits.end(); i++)
+	{
+		if ((*i)->getPlayer() == Broodwar->self() && (*i)->getType().getID() == UnitTypes::Zerg_Zergling)
+			strikers.push_back(*i);
+	}
+}
+
+void Striker::setEnemyBase(Position base)
+{
+	enemyBase = base;
+}
+
+void Striker::updateStrikers(void)
+{
+	if (initialized)
+	{
+		for (vector<Unit*>::iterator i = strikers.begin(); i != strikers.end(); i++)
+		{
+			(*i)->attack(enemyBase);
+		}
+	}
 }
